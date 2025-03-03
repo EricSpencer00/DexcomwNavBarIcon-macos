936024aed6879dd28b410de8c626577ed2ccd6d3
Eric Spencer <espencer2@luc.edu>
2025-03-03 15:29:30 -0600

    change name to app

--- setup.py
+++ setup.py
@@ -7,14 +7,12 @@ from setuptools import setup
 APP = ['app.py']  # replace with your filename
 DATA_FILES = []
 OPTIONS = {
-    # 'argv_emulation': True,
-    # 'excludes': ['ctypes'],
-    # 'frameworks': ['/System/Library/Frameworks/Carbon.framework,', '/System/Library/Frameworks/Cocoa.framework'],
     'alias': True,              # uses symlinks instead of copying resources
     'iconfile': 'icon.icns',    # use a proper .icns file for the macOS app icon
 }
 
 setup(
+    name="DexcomNavBarIcon",
     app=APP,
     data_files=DATA_FILES,
     options={'py2app': OPTIONS},
